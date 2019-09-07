3fcc46447696e494ec15241b515da4fddcc37d52
Jonas Hertzman <jonas.hertzman@gmail.com>
2019-09-07 16:13:01 +0200

    Add project files.

---
+++ PhotoMover/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace PhotoMover
+{
+	public partial class MainWindow : Window
+	{
+
+		#region Constructor
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Events
+
+		private void Window_Initialized(object sender, System.EventArgs e)
+		{
+
+		}
+
+		private void Window_ContentRendered(object sender, System.EventArgs e)
+		{
+
+		}
+
+		private void Window_Closed(object sender, System.EventArgs e)
+		{
+
+		}
+
+		#endregion
+
+	}
+}
