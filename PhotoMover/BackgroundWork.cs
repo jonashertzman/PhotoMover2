ec4dfa1547ed15ab5c27a2c21e647b6d4cb2599a
Jonas Hertzman <jonas.hertzman@gmail.com>
2019-09-16 21:17:22 +0200

    Background threading, commands, about window

---
+++ PhotoMover/BackgroundWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace PhotoMover
+{
+	public static class BackgroundWork
+	{
+
+		private static int progress;
+		public static IProgress<int> progressHandler;
+		private static DateTime startTime;
+		private static string currentRoot;
+
+		public static void FindFiles()
+		{
+			Thread.Sleep(2000);
+			progressHandler.Report(70);
+			Thread.Sleep(2000);
+		}
+
+	}
+}
--- PhotoMover/MainWindow.xaml.cs
+++ PhotoMover/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Markup;
 
@@ -61,7 +65,10 @@ namespace PhotoMover
 
 		private void Window_ContentRendered(object sender, System.EventArgs e)
 		{
-
+			if (Environment.GetCommandLineArgs().Length > 1)
+			{
+				ViewModel.ImportPath = Environment.GetCommandLineArgs()[1];
+			}
 		}
 
 		private void Window_Closed(object sender, System.EventArgs e)
@@ -97,16 +104,66 @@ namespace PhotoMover
 			}
 		}
 
-
-		private void ButtonAnalyze_Click(object sender, RoutedEventArgs e)
+		private void ButtonFindFiles_Click(object sender, RoutedEventArgs e)
 		{
-			ViewModel.ImportFiles.Add(new ImportFile() { SourcePath = "A", DestinationPath = "B", Status = "C" });
+			if (Directory.Exists(ViewModel.ImportPath))
+			{
+				ViewModel.GuiFrozen = true;
+
+				ProgressBarWork.Value = 0;
+
+				Debug.Print("------ ButtonFindFiles_Click");
+				BackgroundWork.progressHandler = new Progress<int>(FindFilesStatusUpdate);
+				Task.Run(() => BackgroundWork.FindFiles()).ContinueWith(AnalyzeFinnished, TaskScheduler.FromCurrentSynchronizationContext());
+			}
+			else
+			{
+				MessageBox.Show("Import folder does not exist.", "Error");
+			}
+		}
+
+		private void FindFilesStatusUpdate(int progress)
+		{
+			Debug.Print("------ FindFilesStatusUpdate");
+			ProgressBarWork.Value = progress;
+		}
+
+		private void AnalyzeFinnished(Task task)
+		{
+			Debug.Print("------ CompareFilesFinnished");
+			ViewModel.GuiFrozen = false;
+
 		}
 
 		private void ButtonCopy_Click(object sender, RoutedEventArgs e)
 		{
 
 		}
+
+		#region Commands
+
+		private void CommandExit_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void CommandAbout_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+		{
+			AboutWindow aboutWindow = new AboutWindow() { Owner = this };
+			aboutWindow.ShowDialog();
+		}
+
+		private void CommandOpenContainingFolder_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+		{
+
+		}
+
+		private void CommandOpenContainingFolder_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+		{
+
+		}
+
+		#endregion
 
 		#endregion
 
