0b3659e993ce5db99c4738a7fb5b38622da2c213
Jonas Hertzman <jonas.hertzman@gmail.com>
2022-12-29 13:13:43 +0100

    New console project

---
+++ PhotoMoverConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PhotoMoverConsole;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine("works");
+			Thread.Sleep(500);
+		}
+
+
+	}
+}
