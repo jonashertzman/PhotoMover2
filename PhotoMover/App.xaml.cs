632bc2a8656793aa37228100789cb8c6a2572fa8
Jonas Hertzman <jonas.hertzman@gmail.com>
2023-01-03 10:16:11 +0100

    Better debugging

--- PhotoMover/App.xaml.cs
+++ PhotoMover/App.xaml.cs
@@ -12,18 +12,27 @@ public partial class App : Application
 
 		if (namedArgs.ContainsKey("--no_gui"))
 		{
+			bool consoleAllocated = false;
+
 			if (!WinApi.AttachConsole(WinApi.ATTACH_PARENT_PROCESS))
 			{
-				// If the application is started from a shortcut, AttachConsole has no console to attach,
-				// We must create the a new console window to see the output.
-				WinApi.AllocConsole();
+				// If the application is started from a shortcut or we are debugging the application in Visual Studio,
+				// AttachConsole has no console to attach to and we must create the a new console window to see the output.
+				consoleAllocated = WinApi.AllocConsole();
 			}
+
 			for (int i = 0; i < 10; i++)
 			{
 				Console.WriteLine(i);
 				Thread.Sleep(400);
 			}
-			WinApi.FreeConsole();
+
+			if (consoleAllocated)
+			{
+				Console.WriteLine("Press any key to continue.");
+				Console.ReadKey();
+				WinApi.FreeConsole();
+			}
 
 			Environment.Exit(0);
 		}
