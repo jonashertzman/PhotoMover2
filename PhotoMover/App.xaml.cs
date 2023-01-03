f13737a26111001bed8db21e61b2b91c8ef0ffb9
Jonas Hertzman <jonas.hertzman@gmail.com>
2023-01-03 16:20:10 +0100

    Command line parsing

--- PhotoMover/App.xaml.cs
+++ PhotoMover/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 
 namespace PhotoMover;
 
@@ -7,10 +8,10 @@ public partial class App : Application
 
 	protected override void OnStartup(StartupEventArgs e)
 	{
-
+		bool showHelp = new[] { "/?", "-h", "--help" }.Contains(e.Args[0]);
 		Dictionary<string, string> namedArgs = ParseArgs(e.Args);
 
-		if (namedArgs.ContainsKey("--no_gui"))
+		if (namedArgs.ContainsKey("--no_gui") || namedArgs.ContainsKey("-h") || namedArgs.ContainsKey("--help"))
 		{
 			bool consoleAllocated = false;
 
@@ -21,15 +22,35 @@ public partial class App : Application
 				consoleAllocated = WinApi.AllocConsole();
 			}
 
-			for (int i = 0; i < 10; i++)
+			if (e.Args.Length > 0)
 			{
-				Console.WriteLine(i);
-				Thread.Sleep(400);
+				if (new[] { "-h", "--help" }.All(key => namedArgs.ContainsKey(key)))
+				{
+					PrintUsage();
+				}
+
+				else
+				{
+					if (Path.Exists(e.Args[0]))
+					{
+						Import(e.Args[0]);
+					}
+					else
+					{
+						Console.WriteLine($"Import folder {e.Args[0]} does not exist.");
+					}
+				}
 			}
+
+			//for (int i = 0; i < 10; i++)
+			//{
+			//	Console.WriteLine(i);
+			//	Thread.Sleep(400);
+			//}
 
 			if (consoleAllocated)
 			{
-				Console.WriteLine("Press any key to continue.");
+				Console.WriteLine("\nPress any key to continue.\n");
 				Console.ReadKey();
 				WinApi.FreeConsole();
 			}
@@ -43,7 +64,7 @@ public partial class App : Application
 
 	}
 
-	private static Dictionary<string, string> ParseArgs(string[] args)
+	private Dictionary<string, string> ParseArgs(string[] args)
 	{
 		Dictionary<string, string> namedArgs = new();
 
@@ -53,15 +74,45 @@ public partial class App : Application
 			string value = "";
 			if (key.StartsWith("--"))
 			{
-				if (args.Length > i + 1 && !args[i + 1].StartsWith("--"))
+				if (args.Length > i + 1 && !args[i + 1].StartsWith("-"))
 				{
 					value = args[i + 1];
+					i++;
 				}
 				namedArgs.Add(key.ToLower(), value);
+			}
+			else if (key.StartsWith("-"))
+			{
+				foreach (char c in key[1..])
+				{
+					namedArgs.Add($"-{c}", "");
+				}
+
 			}
 		}
 
 		return namedArgs;
+	}
+
+	private void Import(string importFolder)
+	{
+		Console.WriteLine($"Importing from {importFolder} ...");
+	}
+
+	private void PrintUsage()
+	{
+		string usage =
+$$"""
+Usage:
+
+{{Path.GetFileName(Environment.ProcessPath)}} [import-folder] [--no_gui] [-h | --help]
+
+import-folder  Folder to import from.
+--no_gui       Run the import in the console.
+-h --help      Shows this message.
+""";
+
+		Console.WriteLine(usage);
 	}
 
 }
